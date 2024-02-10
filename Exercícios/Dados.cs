6a6958a08cde46aa53bc34340d2efa5f2ffa92d5
Alexvt23 <amsv1@aluno.ifal.edu.br>
2024-02-10 17:58:54 -0300

    Adicionar arquivos de projeto.

---
+++ Exercícios/Dados.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios
+{
+    internal class Dados
+    {
+
+        //atributos 
+        private string cliente;
+        private string senha;
+        private int saldo;
+
+        //propriedades
+
+        public string Cliente
+        { 
+            get { return cliente; } 
+            set {  cliente = value; } 
+        }
+
+        public string Senha
+        { 
+            get { return senha; } 
+            set { senha = value; } 
+        }    
+
+        public int Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+        //métodos
+
+        public void mSaldo(string pSenha)
+        {
+            if (senha==pSenha)
+            {
+                Console.WriteLine($"Seu saldo é: {Saldo.ToString("c")}");
+            }
+            else 
+            {
+                Console.WriteLine("Senha incorreta.");
+            }
+        }
+
+
+        public int mSaque(string pSenha, int pValor)
+        {
+            if (senha==pSenha)
+            {
+                if(Saldo >=pValor)
+                {
+                    Console.WriteLine($"Saque realizado com sucesso.");
+                    Console.WriteLine($"Valor: {pValor.ToString("c")}");
+                    saldo -=pValor;
+                    return pValor;
+                }
+                else 
+                {
+                    Console.WriteLine("Saldo insuficiente.");
+                    return 0;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Senha incorreta.");
+                return 0;
+            }
+        }
+
+
+        public int mDeposito(string pCliente, int pDeposito)
+        {
+            if (Cliente==pCliente)
+            {
+                Console.WriteLine("Depósito realizado com sucesso.");
+                saldo += pDeposito;
+                return pDeposito;
+            }
+            else
+            {
+                Console.WriteLine("Cliente não localizado.");
+                return 0;
+            }
+        }
+
+
+        //construtor
+        public Dados (string pcliente, string pSenha)
+        {        
+            cliente = pcliente;
+            senha = pSenha;
+            saldo = 0;
+        }
+
+    }
+}
---
+++ Exercícios/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dados cliente = new Dados("Alex Mateus", "2206");
+
+           
+
+            while (true)
+            {
+                Console.WriteLine("---Menu---");
+                Console.WriteLine("1 - Saldo");
+                Console.WriteLine("2 - Saque");
+                Console.WriteLine("3 - Depósito");
+                Console.WriteLine("4 - Sair");
+
+                Console.WriteLine("Digite o número da sua opção");
+                string opcao = Console.ReadLine();
+
+                Console.Clear();    
+
+
+                if (opcao == "1")
+                {
+                    Console.WriteLine("Informe sua senha");
+                    string senha = Console.ReadLine();
+
+                    Console.Clear();
+
+                    cliente.mSaldo(senha);
+                  
+                }
+
+                else if (opcao == "2") 
+                {
+                    
+                    Console.WriteLine("Informe o valor do saque");
+                    int saque = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Informe sua senha");
+                    string senha = Console.ReadLine();
+
+                    Console.Clear();
+
+                    cliente.mSaque(senha, saque);
+                }
+
+                else if (opcao == "3") 
+                {
+                    Console.WriteLine("Informe o valor de depósito");
+                    int valor = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Informe o nome dp cliente");
+                    string nome = Console.ReadLine();
+
+                    Console.Clear();
+
+                    cliente.mDeposito(nome, valor);
+                }
+
+                else if (opcao == "4") 
+                {
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Opção desconhecida.");
+                }
+            }
+
+            
+
+        }
+
+      
+    }
+}
